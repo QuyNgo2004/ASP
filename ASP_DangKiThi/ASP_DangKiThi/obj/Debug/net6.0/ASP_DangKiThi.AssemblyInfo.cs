fd4534e5af9e37692ce73d70defb57ee4029531a
22211tt4550 <22211tt4550@mail.tdc.edu.vn>
2024-10-15 11:20:43 +0700

    Re Update

--- ASP_DangKiThi/ASP_DangKiThi/obj/Debug/net6.0/ASP_DangKiThi.AssemblyInfo.cs
+++ ASP_DangKiThi/ASP_DangKiThi/obj/Debug/net6.0/ASP_DangKiThi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASP_DangKiThi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14803a78418685f07ff8e55f257d194a7d79e183")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ef7d6548bc3f0838b2282a0bc0a63bafb5b4d26")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASP_DangKiThi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASP_DangKiThi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
