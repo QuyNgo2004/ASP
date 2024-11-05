76221bc35931189b35da32e42eadef56e408b96e
22211tt4550 <22211tt4550@mail.tdc.edu.vn>
2024-11-05 12:43:36 +0700

    Thêm mới chức năng phòng học

--- DangKyHocThuat_Web/DangKyHocThuat_Web/Controllers/ClassController.cs
+++ DangKyHocThuat_Web/DangKyHocThuat_Web/Controllers/ClassController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Mvc;
 using X.PagedList.Extensions;
 using X.PagedList;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using DangKyHocThuat_Web.Models.ViewModels;
 
 namespace DangKyHocThuat_Web.Controllers
 {
@@ -20,10 +22,18 @@ namespace DangKyHocThuat_Web.Controllers
             IPagedList<Class> v_arrData = new List<Class>().ToPagedList(1, 10);
             try
             {
-               v_arrData = _context.Classes
-             .Where(it => it.Deleted == 0)
-             .OrderBy(p => p.ClassName) // Thêm sắp xếp để có kết quả nhất quán
-             .ToPagedList(p_iPage ?? 1, 10);// Phân trang với trang mặc định là 1
+                v_arrData = _context.Classes
+              .Where(it => it.Deleted == 0)
+              .OrderBy(p => p.ClassName) // Thêm sắp xếp để có kết quả nhất quán
+              .ToPagedList(p_iPage ?? 1, 10);// Phân trang với trang mặc định là 1
+
+                foreach (Class v_objItem in v_arrData)
+                {
+                    v_objItem.Professor = _context.Professors.FirstOrDefault(it => it.AutoId == v_objItem.ProfessorId);
+
+                    if (v_objItem.Professor == null)
+                        v_objItem.Professor = new Professor();
+                }
             }
             catch (Exception ex)
             {
@@ -37,12 +47,24 @@ namespace DangKyHocThuat_Web.Controllers
 
         public IActionResult Edit(int id)
         {
-            Class v_objData = new Class();
+            ClassViewModel v_objData = new ClassViewModel();
             try
             {
-                v_objData = _context.Classes.FirstOrDefault(it => it.AutoId == id);
-                if (v_objData == null)
-                    v_objData = new Class();
+                Class v_objEdit = _context.Classes.FirstOrDefault(it => it.AutoId == id);
+                if (v_objEdit == null)
+                    v_objEdit = new Class();
+
+                v_objData.AutoId = v_objEdit.AutoId;
+                v_objData.ClassName = v_objEdit.ClassName;
+                v_objData.ProfessorId = v_objEdit.ProfessorId;
+                v_objData.Deleted = v_objEdit.Deleted;
+                v_objData.CreatedAt = v_objEdit.CreatedAt;
+                v_objData.CreatedBy = v_objEdit.CreatedBy;
+                v_objData.UpdatedAt = v_objEdit.UpdatedAt;
+                v_objData.UpdatedBy = v_objEdit.UpdatedBy;
+
+                //Combobox professor
+                v_objData.Professors = _context.Professors.ToList();
             }
             catch (Exception ex)
             {
@@ -100,7 +122,7 @@ namespace DangKyHocThuat_Web.Controllers
         #region Sự kiện nhấn form lưu
 
         [HttpPost]
-        public IActionResult Save_Data(Class p_objRequest)
+        public IActionResult Save_Data(ClassViewModel p_objRequest)
         {
             try
             {
@@ -119,7 +141,7 @@ namespace DangKyHocThuat_Web.Controllers
             return View("Edit", p_objRequest);
         }
 
-        private void Update_Data(Class p_objRequest)
+        private void Update_Data(ClassViewModel p_objRequest)
         {
             if (p_objRequest.ClassName.Trim() == "")
                 throw new Exception("Mã lớp không được để trống.");
@@ -136,11 +158,21 @@ namespace DangKyHocThuat_Web.Controllers
             p_objRequest.UpdatedBy = "tuantm";
             p_objRequest.UpdatedAt = DateTime.Now;
 
-            _context.Classes.Update(p_objRequest);
+            Class v_objData = new Class();
+            v_objData.AutoId = p_objRequest.AutoId;
+            v_objData.ClassName = p_objRequest.ClassName;
+            v_objData.ProfessorId = p_objRequest.ProfessorId;
+            v_objData.Deleted = p_objRequest.Deleted;
+            v_objData.CreatedAt = p_objRequest.CreatedAt;
+            v_objData.CreatedBy = p_objRequest.CreatedBy;
+            v_objData.UpdatedAt = p_objRequest.UpdatedAt;
+            v_objData.UpdatedBy = p_objRequest.UpdatedBy;
+
+            _context.Classes.Update(v_objData);
             _context.SaveChanges();
         }
 
-        private void Add_Data(Class p_objRequest)
+        private void Add_Data(ClassViewModel p_objRequest)
         {
             if (p_objRequest.ClassName.Trim() == "")
                 throw new Exception("Mã lớp không được để trống.");
@@ -154,16 +186,39 @@ namespace DangKyHocThuat_Web.Controllers
             if (v_objCheck != null)
                 throw new Exception("Lớp đã tồn tại.");
 
-            p_objRequest.Deleted = 0;
-            p_objRequest.CreatedBy = "tuantm";
-            p_objRequest.CreatedAt = DateTime.Now;
             p_objRequest.UpdatedBy = "tuantm";
             p_objRequest.UpdatedAt = DateTime.Now;
 
-            _context.Classes.Add(p_objRequest);
+            Class v_objData = new Class();
+            v_objData.AutoId = p_objRequest.AutoId;
+            v_objData.ClassName = p_objRequest.ClassName;
+            v_objData.ProfessorId = p_objRequest.ProfessorId;
+            v_objData.Deleted = p_objRequest.Deleted;
+            v_objData.CreatedAt = DateTime.Now;
+            v_objData.CreatedBy = "tuantm";
+            v_objData.UpdatedAt = DateTime.Now;
+            v_objData.UpdatedBy = "tuantm";
+
+            _context.Classes.Add(v_objData);
             _context.SaveChanges();
         }
 
         #endregion
+
+        private List<SelectListItem> ListProfessors()
+        {
+            List<SelectListItem> v_arrRes = new List<SelectListItem>();
+
+            List<Professor> v_arrProfessor = _context.Professors.ToList();
+            foreach (Professor v_objItem in v_arrProfessor)
+            {
+                SelectListItem v_objItem_Combo = new SelectListItem(text: v_objItem.ProfessorId + '|' + v_objItem.ProfessorEmail,
+                    value: v_objItem.AutoId.ToString());
+
+                v_arrRes.Add(v_objItem_Combo);
+            }
+
+            return v_arrRes;
+        }
     }
 }
---
+++ DangKyHocThuat_Web/DangKyHocThuat_Web/Models/ViewModels/ClassViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace DangKyHocThuat_Web.Models.ViewModels
+{
+    public class ClassViewModel
+    {
+        public int AutoId { get; set; } = 0;
+        public string ClassName { get; set; } = "";
+        public int ProfessorId { get; set; } = 0;
+        public int Deleted { get; set; } = 0;
+        public DateTime CreatedAt { get; set; } = new DateTime();
+        public string CreatedBy { get; set; } = "";
+        public DateTime UpdatedAt { get; set; } = new DateTime();
+        public string UpdatedBy { get; set; } = "";
+
+        public List<Professor> Professors { get; set; } = new List<Professor>();
+    }
+}
